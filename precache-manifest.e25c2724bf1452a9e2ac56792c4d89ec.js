c4e25824f3358ba44f7460d36e97ce220cc70ba8
Areum Jo <areumjo1@gmail.com>
2020-05-05 10:47:22 -0700

    Updates

---
+++ precache-manifest.e25c2724bf1452a9e2ac56792c4d89ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b28fe8d7240810ff5c0c1a12eaf1449",
+    "url": "/fcc-07-random-quote/index.html"
+  },
+  {
+    "revision": "34fef2d1c14c5a8fed61",
+    "url": "/fcc-07-random-quote/static/css/main.bae3880e.chunk.css"
+  },
+  {
+    "revision": "4109ce58a74b73f3091f",
+    "url": "/fcc-07-random-quote/static/js/2.7e7787d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fcc-07-random-quote/static/js/2.7e7787d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34fef2d1c14c5a8fed61",
+    "url": "/fcc-07-random-quote/static/js/main.c60535b2.chunk.js"
+  },
+  {
+    "revision": "967678ff05d40ed7f30e",
+    "url": "/fcc-07-random-quote/static/js/runtime-main.33630a82.js"
+  }
+]);
